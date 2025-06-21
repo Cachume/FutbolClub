ceef5604fd816b269cf98ccbc85c849bb899e886
Cachume <albertq703@gmail.com>
2025-06-21 18:24:39 -0400

    Modiify header admin

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 const submenudrop = document.querySelectorAll(".menu-dropdown");
+const barralateral = document.getElementById("barralateral");
+const menu_btn = document.getElementById("btn-menu");
+
 
 submenudrop.forEach(submenuitem => {
     submenuitem.addEventListener("click", function() {
         submenuitem.classList.toggle("toggle");
     });
 });
+
+menu_btn.addEventListener("click", function(){
+    barralateral.classList.toggle("oculto");
+    document.getElementById("index-main").classList.toggle("oculto");
+})
