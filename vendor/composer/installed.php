6031b5e61d255ffb92e32f1c93454c274c0ea3d7
Cachume <albertq703@gmail.com>
2025-10-07 23:50:45 -0400

    librerias

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cachume/futbol-club',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f9558beca7b70892f297fd2cd36d6fd5c9d1ad66',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cachume/futbol-club' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f9558beca7b70892f297fd2cd36d6fd5c9d1ad66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
